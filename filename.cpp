1490d82ad58735e291626217a865c49fda4bd311
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-03 00:54:44 +0500

    Setting the Source code of the Vertex and Fragment Shader

--- filename.cpp
+++ filename.cpp
@@ -1,6 +1,7 @@
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -19,6 +20,24 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     }
 
 }
+/*
+    Source Code of the Vertex Shader and Fragment Shader, the reason 
+    we are creating the Source code of vertex and Fragment is that
+    because by default the Source code does not exit in our OpenGL.
+*/
+const char* vertexShaderSource="#version 330 core\n"
+"layout (location=0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position=vec4(aPos.x,aPos.y,aPos.z,1.0)\n;"
+"}\0";
+
+const char* fragmentShaderSource="#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+      "FragColor=vec4(0.8f,0.3f,0.02f,1.0f);\n"
+"}\0";
 
 int main() 
 {
@@ -31,7 +50,7 @@ int main()
 
   int width=0,height=0;
 
-  // uncomment these lines if on Apple OS X
+  //Telling which OpenGL version should we be using 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -60,15 +79,34 @@ int main()
   glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
 
 
+
+  /* OTHER STUFF GOES HERE NEXT */
   glfwGetFramebufferSize(window,&width,&height);
   glViewport(0,0,width,height);  
-  cout<<width<<" "<<height<<endl;
-  /* OTHER STUFF GOES HERE NEXT */
-  float points[]={
-    0.0f,0.5f,0.0f,
-    0.5f,-0.5f,0.0f,
-    -0.5f,-0.5f,0.0f,    
+  GLfloat vertices[]=
+  {
+     -0.5f,-0.5f*float(sqrt(3))/3,0.0f,
+      0.5f,-0.5f*float(sqrt(3))/3,0.0f,
+      0.0f,-0.5f*float(sqrt(3))*2/3,0.0f,
   };
+
+  GLuint vertexShader=glCreateShader(GL_VERTEX_SHADER);//Getting the Vertex Shader from the memmory
+  glShaderSource(vertexShader,1,&vertexShaderSource,NULL);//Creating the Source Code of the Vertex Shader
+  glCompileShader(vertexShader);//Compilling the Vertex Shader Source Code
+
+  GLuint fragmentShader=glCreateShader(GL_FRAGMENT_SHADER);//Getting the fragment Shader from the memmory
+  glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);//Creating the Source the code Vertex Shader
+  glCompileShader(fragmentShader);//Compilling the Source Code of the Fragment Shader
+  /*
+     Now we trying to wrap the vertex shader and fragment shader in a single shadder
+  */
+  GLuint shaderProgram=glCreateProgram();//Creating the shader program
+  glAttachShader(shaderProgram,vertexShader);//Attaching the Vertex Shader with the Shader Program
+  glAttachShader(shaderProgram,fragmentShader);//Attaching the Fragment Shader with the Shader Program
+  glLinkProgram(shaderProgram);
+
+   
+
 
   glClearColor(0.07f,0.13f,0.17f,1.0f);
   while(!glfwWindowShouldClose(window))
